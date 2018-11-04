6dbc8d23fea3557528f4a61d9f1413b5b4692e48
caoxingyu <1746569077@qq.com>
2018-11-04 21:25:14 +0800

    错误信息修改

--- src/main/java/com/milktea/milkteashop/exception/MilkTeaErrorConstant.java
+++ src/main/java/com/milktea/milkteashop/exception/MilkTeaErrorConstant.java
@@ -13,11 +13,11 @@ public enum MilkTeaErrorConstant {
     
     PARAMETER_REQUIRED("00002", "请求参数不能为空", "Parameter required"),
     
-    CN_CLASS_NAME_REQUIRED("00003", "分类名称（中文）不能为空", "Class name required"),
+    CN_CLASS_NAME_REQUIRED("00003", "分类名称不能为空", "Class name required"),
     
     US_CLASS_NAME_REQUIRED("00004", "分类名称（英文）不能为空", "Class name required"),
     
-    CN_CLASS_LOGO_REQUIRED("00005", "分类logo（中文）不能为空", "Class logo required"),
+    CN_CLASS_LOGO_REQUIRED("00005", "分类logo不能为空", "Class logo required"),
     
     US_CLASS_LOGO_REQUIRED("00006", "分类logo（英文）不能为空", "Class logo required"),
     
@@ -25,7 +25,7 @@ public enum MilkTeaErrorConstant {
     
     CLASS_ID_REQUIRED("00008", "分类ID不能为空", "Class id required"),
     
-    CN_ATTR_NAME_REQUIRED("00009", "商品属性名称（中文）不能为空", "Attribute name required"),
+    CN_ATTR_NAME_REQUIRED("00009", "商品属性名称不能为空", "Attribute name required"),
     
     US_ATTR_NAME_REQUIRED("00010", "商品属性名称（英文）不能为空", "Attribute name required"),
     
@@ -33,7 +33,7 @@ public enum MilkTeaErrorConstant {
     
     ATTR_ID_REQUIRED("00012", "商品规格ID不能为空", "Attribute id required"),
     
-    CN_GOODS_NAME_REQUIRED("00013", "商品名称（中文）不能为空", "Goods name required"),
+    CN_GOODS_NAME_REQUIRED("00013", "商品名称不能为空", "Goods name required"),
     
     US_GOODS_NAME_REQUIRED("00014", "商品名称（英文）不能为空", "Goods name required"),
     
@@ -45,13 +45,13 @@ public enum MilkTeaErrorConstant {
     
     STORE_NO_REQUIRED("00018", "店铺编号不能为空", "StoreNo required"),
     
-    CN_PROMOTION_NAME_REQUIRED("00019", "活动名称（中文）不能为空", "Promotion name required"),
+    CN_PROMOTION_NAME_REQUIRED("00019", "活动名称不能为空", "Promotion name required"),
     
     US_PROMOTION_NAME_REQUIRED("00020", "活动名称（英文）不能为空", "Promotion name required"),
     
     PROMOTION_ID_REQUIRED("00021", "活动ID不能为空", "Promotion id required"),
     
-    CN_FIGURE_ADDRESS_REQUIRED("00022", "轮播图地址（中文）不能为空", "Figure address required"),
+    CN_FIGURE_ADDRESS_REQUIRED("00022", "轮播图地址不能为空", "Figure address required"),
     
     US_FIGURE_ADDRESS_REQUIRED("00023", "轮播图地址（英文）不能为空", "Figure address required"),
     
@@ -63,7 +63,7 @@ public enum MilkTeaErrorConstant {
     
     USER_NAME_OR_PASSWORD_WRONG("00027", "用户名或密码错误", "User name or password wrong"),
     
-    CN_STORE_NAME_REQUIRED("00028", "店铺名称（中文）不能为空", "Store name required"),
+    CN_STORE_NAME_REQUIRED("00028", "店铺名称不能为空", "Store name required"),
     
     US_STORE_NAME_REQUIRED("00029", "店铺名称（英文）不能为空", "Store name required"),
     
@@ -73,7 +73,7 @@ public enum MilkTeaErrorConstant {
     
     STORE_STATUS_REQUIRED("00032", "店铺状态不能为空", "Store status required"),
     
-    CN_GOODS_NAME_EXISTS("00033", "商品名称(中文)重复", "Goods name already exists"),
+    CN_GOODS_NAME_EXISTS("00033", "商品名称重复", "Goods name already exists"),
     
     US_GOODS_NAME_EXISTS("00034", "商品名称(英文)重复", "Goods name already exists"),
     
@@ -87,11 +87,11 @@ public enum MilkTeaErrorConstant {
     
     GOODS_STOCK_UNMAINTAINED("00039", "商品库存未维护", "Goods stock unmaintained"),
     
-    CN_STORE_NAME_EXISTS("00040", "店铺名称(中文)重复", "Store name already exists"),
+    CN_STORE_NAME_EXISTS("00040", "店铺名称重复", "Store name already exists"),
     
     US_STORE_NAME_EXISTS("00041", "店铺名称(英文)重复", "Store name already exists"),
     
-    CN_ATTR_NAME_EXISTS("00042", "属性名称(中文)重复", "Attr name already exists"),
+    CN_ATTR_NAME_EXISTS("00042", "属性名称重复", "Attr name already exists"),
     
     US_ATTR_NAME_EXISTS("00043", "属性名称(英文)重复", "Attr name already exists"),
     
@@ -121,7 +121,7 @@ public enum MilkTeaErrorConstant {
     
     CONTEXT_REQUIRED("00056", "内容不能为空", "Context required"),
     
-    CN_CLASS_NAME_EXISTS("00057", "分类名称（中文）已存在", "Class name already exists"),
+    CN_CLASS_NAME_EXISTS("00057", "分类名称已存在", "Class name already exists"),
     
     US_CLASS_NAME_EXISTS("00058", "分类名称（英文）已存在", "Class name already exists"),
 
